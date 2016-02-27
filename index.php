a2c9263d0f5526b5fce5c266ff41af08af839ead
Vladislav <v.kirbaba@gmail.com>
2016-02-27 12:19:10 +0300

    Хедер

--- index.php
+++ index.php
@@ -9,9 +9,72 @@
     <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
-
 <body>
-    
+    <!-- open .header -->
+    <header class="header">
+    	<!-- open .container -->
+    	<div class="container">
+    		<!-- open .row -->
+    		<div class="row">
+    			<!-- open .col-lg-4 col-md-4 col-sm-5 col-xs-12 -->
+    			<div class="col-lg-4 col-md-4 col-sm-5 col-xs-12">
+    				<!-- open .header__title -->
+    				<div class="header__title">
+    					<a href="#" class="header__title--logo">
+    						<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="" />
+    					</a>
+    					<h2><i>Ренмедиа</i></h2>
+    					<p><i>Ваша реклама в Раменском<br />на канале РЕН ТВ</i></p>
+    				</div>
+    				<!-- close .header__title -->
+    				
+    			</div>
+    			<!-- close .col-lg-4 col-md-4 col-sm-5 col-xs-12 -->
+    			<!-- open .col-lg-4 col-md-4 col-sm-5 col-xs-12 -->
+    			<div class="col-lg-5 col-md-5 col-sm-5 col-lg-offset-3 col-md-col-md-offset-3 col-sm-offset-2 col-xs-12">
+    				<!-- open .header__contacts -->
+    				<div class="header__contacts">
+    					<h3>+7 499 390-92-20</h3>
+    					<a href="#" class="header__contacts--order-call">Заказать обратный звонок</a>
+    					<h4>info@renmedia-tv.ru</h4>
+    					<p>Московская область, Раменское, ул. Михалевича, 49</p>
+    				</div>
+    				<!-- close .header__contacts -->
+    			</div>
+    			<!-- close .col-lg-4 col-md-4 col-sm-5 col-xs-12 -->
+    			<nav class="navbar navbar-default navigation" role="navigation">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>								 
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">		      	     
+							<ul class="nav navbar-nav navigation__list">		      
+								<li><a href="#go_about" class="smoothScroll">Пункт 1</a></li>
+								<li><a href="#go_prod" class="smoothScroll">Пункт 1</a></li>
+								<li><a href="#go_lic" class="smoothScroll">Пункт 1</a></li>
+								<li><a href="#go_chen" class="smoothScroll">Пункт 1</a></li>
+								<li><a href="#go_cont" class="smoothScroll">Пункт 1</a></li>
+								<li><a href="#go_cont" class="smoothScroll">Пункт 1</a></li>	
+								<li><a href="#go_cont" class="smoothScroll">Контакты</a></li>		      
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+    		</div>
+    		<!-- close .row -->
+    	</div>
+    	<!-- close .container -->
+    </header>
+    <!-- close .header -->
+
+    video
 <?php wp_footer(); ?>
 </body>
 </html>
